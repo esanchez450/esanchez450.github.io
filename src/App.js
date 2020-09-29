bce5a30faff9d668c15ff60d3908888c678f93a4
Manny <sancheze0450@gmail.com>
2020-09-29 01:56:50 -0400

    final commit, I am the React guru

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/esanchez450"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Come Holla at Me!!
         </a>
       </header>
     </div>
